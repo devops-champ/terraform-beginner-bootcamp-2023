49d89953d1fc786cbd6f1d9056e49b66f2fa19a3
Darshan Nag <dndarshan2@gmail.com>
2025-01-03 10:11:37 +0000

    aws s3 bucket creation

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.3"
     }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }    
   }
 }
 
@@ -12,11 +17,17 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
+  upper = false
+  lower = true
   special          = false
 
 }
 
+resource "aws_s3_bucket" "bucket_name" {
+  bucket = random_string.bucket_name.result
+  
+}
 output "random_bucket_name" {
   value = random_string.bucket_name.result
 }
